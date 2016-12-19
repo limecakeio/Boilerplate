d08bd169774a43046afa4043ff6cc7076f8ff097
Simon <black-simon@hotmail.de>
2016-12-19 23:48:22 +0100

    Router erweitert(Get+Post Requests)

--- CNC/Server/FetchTry.js
+++
@@ -1,9 +0,0 @@
-var fetch = require('node-fetch');
-
-
-fetch('http://localhost:3000/api/Status',{methdod : 'GET'})
-    .then(function(res) {
-        return res.json();
-    }).then((json) => {
-        console.log(json);
-    });
